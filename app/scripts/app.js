2fe2852b4e7ccabf123d7c42726109a4c508dfaa
Vitalij Kudresov <kudresov@gmail.com>
2014-06-26 15:40:37 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name test4App
+ * @description
+ * # test4App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('test4App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
